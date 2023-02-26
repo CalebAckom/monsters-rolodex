d165c3cd082708142acb8eb6c17cb7469570776f
CalebAckom <calebackom@amalitech.com>
2023-02-26 17:37:42 +0000

    Adding components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Using a class component, we get access to State
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: "Leanne Graham",
+          id: "1",
+          email: "leanne.graham@mail.com"
+        },
+        {
+          name: "Ervin Howell",
+          id: "2",
+          email: "ervin.howell@mail.com"
+        },
+        {
+          name: "Clementine Bauch",
+          id: "3",
+          email: "clementine.bauch@mail.com"
+        },
+        {
+          name: "Patricia Lebsack",
+          id: "4",
+          email: "patricia.lebsack@mail.com"
+        },
+        {
+          name: "Chelsey Dietrich",
+          id: "5",
+          email: "chelsey.dietrich@mail.com"
+        },
+        {
+          name: "Mrs. Dennis Schulist",
+          id: "6",
+          email: "dennis.schulist.mail.com"
+        },
+        {
+          name: "Kurtis Weissnat",
+          id: "7",
+          email: "kurtis.weissnat@mail.com"
+        },
+        {
+          name: "Nicholas Runolfsdottir V",
+          id: "8",
+          email: "nicholas.runolfsddottir@mail.com"
+        },
+        {
+          name: "Glenna Reichert",
+          id: "9",
+          email: "glenna.reichert@mail.com"
+        },
+        {
+          name: "Clementina DuBuque",
+          id: "10",
+          email: "clementina.dubuque@mail.com"
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CardList monsters={this.state.monsters} />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/sampleData.js
@@ -0,0 +1,119 @@
+const monsters = [
+    {
+        id: '1',
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "sincere@april.biz",
+        address: {
+            street: "Kulas Light",
+            suite: "Apt. 556",
+            city: "Gwenborough",
+            zipcode: "92998-3874",
+            geo: {
+                lat: "-37.3159",
+                lng: "81.1496"
+            }
+        },
+        phone:  "1-770-736-8031 x56442",
+        website:    "hildegard.org",
+        company:    {
+            name:   "AmaliTech",
+            catchPhrase:    "The quick brown fox",
+            bs: "Digital Innovativeness"
+        }
+    },
+    {
+        id: '2',
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "sincere@april.biz",
+        address: {
+            street: "Kulas Light",
+            suite: "Apt. 556",
+            city: "Gwenborough",
+            zipcode: "92998-3874",
+            geo: {
+                lat: "-37.3159",
+                lng: "81.1496"
+            }
+        },
+        phone:  "1-770-736-8031 x56442",
+        website:    "hildegard.org",
+        company:    {
+            name:   "AmaliTech",
+            catchPhrase:    "The quick brown fox",
+            bs: "Digital Innovativeness"
+        }
+    },
+    {
+        id: '3',
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "sincere@april.biz",
+        address: {
+            street: "Kulas Light",
+            suite: "Apt. 556",
+            city: "Gwenborough",
+            zipcode: "92998-3874",
+            geo: {
+                lat: "-37.3159",
+                lng: "81.1496"
+            }
+        },
+        phone:  "1-770-736-8031 x56442",
+        website:    "hildegard.org",
+        company:    {
+            name:   "AmaliTech",
+            catchPhrase:    "The quick brown fox",
+            bs: "Digital Innovativeness"
+        }
+    },
+    {
+        id: '4',
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "sincere@april.biz",
+        address: {
+            street: "Kulas Light",
+            suite: "Apt. 556",
+            city: "Gwenborough",
+            zipcode: "92998-3874",
+            geo: {
+                lat: "-37.3159",
+                lng: "81.1496"
+            }
+        },
+        phone:  "1-770-736-8031 x56442",
+        website:    "hildegard.org",
+        company:    {
+            name:   "AmaliTech",
+            catchPhrase:    "The quick brown fox",
+            bs: "Digital Innovativeness"
+        }
+    },
+    {
+        id: '5',
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "sincere@april.biz",
+        address: {
+            street: "Kulas Light",
+            suite: "Apt. 556",
+            city: "Gwenborough",
+            zipcode: "92998-3874",
+            geo: {
+                lat: "-37.3159",
+                lng: "81.1496"
+            }
+        },
+        phone:  "1-770-736-8031 x56442",
+        website:    "hildegard.org",
+        company:    {
+            name:   "AmaliTech",
+            catchPhrase:    "The quick brown fox",
+            bs: "Digital Innovativeness"
+        }
+    }
+]
+
+module.exports = { monsters }

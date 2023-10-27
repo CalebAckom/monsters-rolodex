3e16cec3f59e482ddc066174f634188fc3171773
CalebAckom <caleb.ackom@amalitech.com>
2023-10-27 07:43:17 +0000

    Restructure IaC folder

---
+++ iac/app/webserver-image.tf
@@ -0,0 +1,13 @@
+resource "docker_image" "monsters-rolodex" {
+  name = "monsters-rolodex:latest"
+
+  build {
+    context = "./../../"
+    dockerfile = "Dockerfile"
+    tag = [var.app]
+    no_cache =  true
+
+  }
+
+  force_remove = true
+}
